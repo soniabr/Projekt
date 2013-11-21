e3ec3b1770e4fe5923d2b11b8689835ccf904224
Sonia Brechbühl <soniabr@student.ethz.ch>
2013-11-21 18:53:51 +0100

    checks agents characteristics 13/11/21

--- code/check_agent.m
+++ code/check_agent.m
@@ -3,10 +3,11 @@ function [agent,Avg] = check_agent( agent, district_agent, tol, runs )
 tic
 k=1;
 
-   Avg=zeros(12,3);
+   Avg=zeros(12,4);
   
 
 for d=1:12                                  % districts
+
  
     l=floor(sqrt(district_agent(d)));       % box(district) length
     lp = sum(district_agent(1:d));          % gives indice of last place in district    
@@ -17,11 +18,10 @@ for d=1:12                                  % districts
         Avg(d,1)=Avg(d,1)+agent(j).origin;       % Avg(1) fraction of swiss residents in district d
         Avg(d,2)=Avg(d,2)+agent(j).age;          % Avg(2) defines average age in district
         Avg(d,3)=Avg(d,3)+agent(j).famstat;      % Avg(3) fraction of families
-        
     end
    
-    Avg(d,:) = Avg(d,:)/(lp-k+1)
- 
+    Avg(d,1:3) = Avg(d,1:3)/(lp-k+1);
+
         
     for i = k:lp                            % agents in district d
     
@@ -54,9 +54,10 @@ for d=1:12                                  % districts
     
     %check district-environment
     
-    agent(i).points = agent(i).points + (1-(agent(i).origin-Avg(1)))*tol(1); % origin
-    agent(i).points = agent(i).points + (1-abs(agent(i).age-Avg(2))/30)*tol(2); % age
-    agent(i).points = agent(i).points + (1-(agent(i).famstat-Avg(3)))*tol(3); % family situation
+    agent(i).points = agent(i).points + (1-(agent(i).origin-Avg(d,1)))*tol(1); % origin
+    agent(i).points = agent(i).points + (1-abs(agent(i).age-Avg(d,2))/30)*tol(2); % age
+    agent(i).points = agent(i).points + (1-(agent(i).famstat-Avg(d,3)))*tol(3); % family situation
+    
     
     
     %Check criteria with neigbours    
@@ -64,16 +65,23 @@ for d=1:12                                  % districts
     for nb=1:4    
         
         %check family status -> +1 if same
-        agent(i).points = agent(i).points + (agent(i).famstat==agent(n(nb)).famstat)/4*tol(3);
-        agent(i).points = agent(i).points + (agent(i).origin==agent(n(nb)).origin)/4*tol(3);
-        agent(i).points = agent(i).points + (round(agent(i).age/10)==round(agent(n(nb)).age/10))/4*tol(3);
+        agent(i).points = agent(i).points + (agent(i).famstat==agent(n(nb)).famstat)/4*tol(4);
+        agent(i).points = agent(i).points + (agent(i).origin==agent(n(nb)).origin)/4*tol(5);
+        agent(i).points = agent(i).points + (round(agent(i).age/10)==round(agent(n(nb)).age/10))/4*tol(6);
+   
+    end
     
     end
     
+  
+       for j=k:lp
     
-    end
-    
-    k=lp+1;                                 %upper boundary of district --> + 1 = lower boudary of next district
+        Avg(d,4)=Avg(d,4)+agent(j).points;  %average points / district
+       end
+        Avg(d,4) = Avg(d,4)/(lp-k+1);
+        
+        
+        k=lp+1;                             %upper boundary of district --> + 1 = lower boudary of next district
 
 end
    
